28b461c30fc2da541bcd1e3d9cdee27338c06c0a
Abdul Wahab <aws18320@gmail.com>
2023-03-22 11:39:57 +0500

    Second Commit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const myEntries = document.querySelectorAll('.hide-card');
+const menu = document.querySelector('.mob-menu');
+const button = document.querySelector('.mob-menu-btn');
+
+// Intersection observer used to animate in elements
 
 const options = {
 	threshold: 0,
@@ -16,3 +20,17 @@ const observer = new IntersectionObserver((entries) => {
 }, options);
 
 myEntries.forEach((el) => observer.observe(el));
+
+// Function to toggle mob menu on clicking hamburger
+
+button.addEventListener('click', () => {
+	menu.classList.toggle('-translate-x-full');
+});
+
+// Function to close the mob menu if clicked outside
+
+window.addEventListener('click', function (e) {
+	if (!menu.contains(e.target) && !button.contains(e.target) && !menu.classList.contains('-translate-x-full')) {
+		menu.classList.add('-translate-x-full');
+	}
+});
